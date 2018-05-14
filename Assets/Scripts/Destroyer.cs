ae73369eb1df48e000b55d69c352ef3f6465d292
Logan Searle <logansearle14@gmail.com>
2018-05-13 20:22:46 -0500

    Initial File Upload

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            // End Scene In Here
+            Debug.Break();
+            return;
+        }
+
+        if (other.gameObject.transform.parent.gameObject)
+        {
+            Destroy(other.gameObject.transform.parent.gameObject);
+        }
+        else
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+
+    public GameObject[] obj;
+    public float spawnMin = 1f;
+    public float spawnMax = 2f;
+    public 
+    
+
+    void Start () {
+        Spawn();
+	}
+
+    void Spawn()
+    {
+        Instantiate(obj[Random.Range (0, obj.GetLength(0))], transform.position, Quaternion.identity);
+        Invoke("Spawn", Random.Range(spawnMin, spawnMax));
+    }
+
+}
