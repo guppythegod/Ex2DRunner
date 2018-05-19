4123606f84a00ead0955bd015f5bce1de43c8f2e
Logan Searle <logansearle14@gmail.com>
2018-05-19 17:12:54 -0500

    Readding files
    Summary

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform player;
+
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(player.position.x + 3, 0, -10);
+	}
+}
